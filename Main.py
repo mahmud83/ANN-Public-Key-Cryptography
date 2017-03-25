cd2cc99e001722d878eb32ef702444089f2b4986
msokoloff1 <matthewsokoloff@gmail.com>
2017-03-25 12:53:55 -0400

    Cleaned up. Finished except for further logging support

--- Main.py
+++ Main.py
@@ -9,8 +9,7 @@ parser = argparse.ArgumentParser(description='Arguments for neural public key ex
 parser.add_argument('-numHidden', default = 10 ,type=int, help = 'Number of hidden nodes for the tree parity machine')
 parser.add_argument('-inputLength',default = 5 ,type=int, help = 'Number of inputs per hidden node for the tree parity machine')
 parser.add_argument('-maxValue', default = 10 ,type=int, help = 'Absolute value for the bounds of the networks weights')
-parser.add_argument('-simple', default = True, type = bool, help = 'Indicates whether or not to run a basic key exchange') 
-parser.add_argument('-attack',default = False, type = bool, help = 'Indicates whether or not to run a key exchange while being attacked')
+parser.add_argument('-attack',default = False, type = str, help = 'Type of attack to perform. Must be either \'none\', \'regular\', or \'geometric\'')
 parser.add_argument('-tensorboard', action = 'store_false',  help = 'Not supported yet')
 parser.add_argument('-concurrent', action = 'store_true',  help = "Run the experiment concurrently or not")
 args = parser.parse_args()
@@ -40,84 +39,46 @@ def expMovGen(logging= True):
 		accuracyC = accuracyC*0.99 + 0.01*updateValueC
 		count += 1
 
-def regularTrain(K, N, L, queue,id):
+
+
+#supported types: "none", "regular", "geometric"
+#TODO: "genetic attack"
+def exchangeKeys(K, N, L, queue,id, type = "none"):
 	np.random.seed()
-	treeA, treeB  = getTrees(K,N,L,numTrees = 2)
+	treeA, treeB, treeC  = getTrees(K,N,L,numTrees = 3)
 	accuracyManager = expMovGen(logging = True)
 	accuracyManager.send(None)
-	accuracy = 0.0
-	while(accuracy<0.99):
+	accuracyAB = 0.0
+	outputC = -2
+	while(accuracyAB<0.99):
 		inputs = np.random.randint(-L,L+1, [K, N])
 		hiddenA,outputA = treeA.getActivations(inputs)
 		hiddenB,outputB = treeB.getActivations(inputs)
-		accuracy,_ ,count = accuracyManager.send([float(outputA == outputB), -1.0])
-		treeA.updateWeights(inputs, hiddenA, outputA, outputB)
-		treeB.updateWeights(inputs, hiddenB, outputB, outputA)
-	print("\nIt took %s iterations for the networks to synchronize."%str(count))
-	if(queue != None):
-		queue.put(count)
-
-	return [accuracy, count]
-
-def trainWithGeometricAttack(K,N,L, queue, id):
-	np.random.seed()
-	treeA, treeB, treeC = getTrees(K,N,L,numTrees=3)
-	accuracyManager = expMovGen()
-	accuracyManager.send(None)
-	accuracyAB = 0.0
-	while(accuracyAB<0.99):
-		inputs = np.random.randint(-L, L+1, [K,N])
-		hiddenA, outputA = treeA.getActivations(inputs)
-		hiddenB, outputB = treeB.getActivations(inputs)
-		hiddenC, outputC = treeC.getActivations(inputs)
-        #Flip the bit most likely to be incorrect
-		if(outputA == outputB and outputB != outputC):
-			nonActivated = treeC.getActivations(inputs, nonActivated = True)
-			uncertainIndex = np.argmin(nonActivated)
-			#Flip the bit. No effect if it guessed zero...
-			hiddenC[uncertainIndex] *= -1
-			outputC = np.prod(hiddenC)
-              
+		if(type in ["regular", "geometric"]):
+			hiddenC, outputC = treeC.getActivations(inputs)
+			if(type == "geometric" and outputA == outputB and outputB != outputC):
+				nonActivated = treeC.getActivations(inputs, nonActivated = True)
+				uncertainIndex = np.argmin(nonActivated)
+				hiddenC[uncertainIndex] *= -1
+				outputC = np.prod(hiddenC)
+			
+			treeC.updateWeights(inputs, hiddenC, outputA, int(outputB == outputC))
 		accuracyAB, accuracyC, count = accuracyManager.send([float(outputA == outputB), float(outputA == outputB == outputC)])
-		#Update Logic:
 		treeA.updateWeights(inputs, hiddenA, outputA, outputB)
 		treeB.updateWeights(inputs, hiddenB, outputB, outputA)
-        #Regular attack update after hidden bit flipped
-		treeC.updateWeights(inputs, hiddenC, outputA, int(outputB == outputC))
 	print("\nIt took %s iterations for the networks to synchronize. Eve achieved %s synchronization"%(str(count),str(accuracyC)))
 	if(queue != None):
 		queue.put([count, accuracyC])
 	return [accuracyAB, accuracyC, count ]
 
-def trainWithSimpleAttack(K,N,L,queue, id):
-	np.random.seed()
-	treeA, treeB, treeC = getTrees(K,N,L,numTrees=3)
-	accuracyManager = expMovGen()
-	accuracyManager.send(None)
-	accuracyAB = 0.0
-	while(accuracyAB<0.99):
-		inputs = np.random.randint(-L,L+1, [K, N])
-		hiddenA,outputA = treeA.getActivations(inputs)
-		hiddenB,outputB = treeB.getActivations(inputs)
-		hiddenC,outputC = treeC.getActivations(inputs)
-		accuracyAB,accuracyC, count = accuracyManager.send([float(outputA == outputB), float(outputA == outputB == outputC)])
-		treeA.updateWeights(inputs, hiddenA, outputA, outputB)
-		treeB.updateWeights(inputs, hiddenB, outputB, outputA)
-		treeC.updateWeights(inputs, hiddenC, outputA, int(outputB == outputC))
-	print("\nIt took %s iterations for the networks to synchronize."%str(count))
-	if(queue != None):
-		queue.put([count, accuracyC])
-	return [accuracyAB, accuracyC, count ]
 
-def experiment(numIters, K,N,L, concurrent):
+def experiment(numIters, K,N,L, concurrent, type):
 	countQ = Queue()
 	counts = []
 	eveSync = []
 	start = time.time()
-	trainFunction = trainWithGeometricAttack
 	if(concurrent):
-
-		concurrentGames = [Process(target = trainFunction, args = (K,N,L,countQ, i)) for i in range(numIters)]
+		concurrentGames = [Process(target = exchangeKeys, args = (K,N,L,countQ, i,type)) for i in range(numIters)]
 		for game in concurrentGames:
 			game.start()
 
@@ -130,7 +91,7 @@ def experiment(numIters, K,N,L, concurrent):
                 	eveSync.append(e)
 	else:
 		for i in range(numIters):
-			a,e,c = trainFunction(K,N,L, None, i)
+			a,e,c = exchangeKeys(K,N,L, None, i,type)
 			eveSync.append(e)
 			counts.append(c)
 			
@@ -143,6 +104,7 @@ def experiment(numIters, K,N,L, concurrent):
 	stdDevEve = np.std(np.array(eveSync))
 
 	print("\n|+++++++EXPERIMENT COMPLETE++++++++|")
+	print("|Attack Type                 : %s  |"%(str(type)))
 	print("|Average Iterations Required : %s  |"%str(avgCounts))
 	print("|Standard Deviation AB Sync  : %s  |"%str(stdDev))
 	print("|Average Eve Synchronization : %s  |"%str(avgEve))
@@ -150,4 +112,4 @@ def experiment(numIters, K,N,L, concurrent):
 	print("|Elapsed Time                : %s  |"%str(elapsed))
 	print("|++++++++++++++++++++++++++++++++++|")
 
-experiment(4, K,N,L, args.concurrent)
+experiment(4, K,N,L, args.concurrent, args.attack)
